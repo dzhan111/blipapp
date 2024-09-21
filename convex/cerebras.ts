a6b29f097015963739df3db4d3c968f00818b1db
Eric Zou <ezou626@gmail.com>
2024-09-21 15:12:37 -0400

    add a system prompt

--- convex/cerebras.ts
+++ convex/cerebras.ts
@@ -11,7 +11,11 @@ export const generatePodcastTextAction = action({
   args: { input: v.string() },
   handler: async (_, { input }) => {
     const completionCreateResponse = await client.chat.completions.create({
-        messages: [{ role: 'user', content: input}],
+        
+        messages: [
+            { role: 'user', content: 'Your job is to be the speaker for a story or long-form single-speaker podcast about the topic and genre that the user provides. The user will also provide a podcast name and an optional speaker name. Do not include any metadata or stage directions in the podcast. Start the podcast immediately after this message.'},
+            { role: 'user', content: input}
+        ],
         model: 'llama3.1-8b',
         max_tokens: 8192,
     });
