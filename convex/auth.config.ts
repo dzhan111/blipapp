906a718216c84259fc9fce727234821a726777ab
dzhan111 <zhandavid4@gmail.com>
2024-09-21 10:36:22 -0400

    yuh

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://comic-sparrow-22.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
