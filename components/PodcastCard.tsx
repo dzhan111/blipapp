72da03810941aa05432bfa37153dc1e1779f8744
dzhan111 <zhandavid4@gmail.com>
2024-09-21 21:11:57 -0400

    edits

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,7 +1,6 @@
 import { PodcastCardProps } from '@/types'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React from 'react'
 
 const PodcastCard = ({
   imgUrl, title, description, podcastId
