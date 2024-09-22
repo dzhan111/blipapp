3925b0032b3fcd03a4e2f59fd2bff80d02d1b6af
Eric Zou <ezou626@gmail.com>
2024-09-22 04:19:34 -0400

    remove extra link

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,6 @@ const Header = ({ headerTitle, titleClassName}: { headerTitle?: string; titleCla
       {headerTitle ? (
         <h1 className={cn('text-18 font-bold text-white-1', titleClassName)}>{headerTitle}</h1>
       ): <div />}
-      <Link href="/discover" className="text-16 font-semibold text-orange-1">
-        See all
-      </Link>
     </header>
   )
 }
